3367e3df474c1f2067e377f36bceaaf6a080770e
ELSASTO <sastoquejose1602@gmail.com>
2024-04-26 19:35:13 -0500

    Actualizacion agregado pagina de error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,30 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <div  align="center" justify="center" >
+        <v-row align="center" justify="center">
+            <v-col cols="11"  class="contenedor"  align="center" justify="center">
+                <v-row align="center" justify="center">
+                    <v-col  data-aos="fade-right" data-aos-duration="3000" align="start" justify="center" cols="10" md="5" lg="5" sm="10">
+                        <h1 class="font1" v-if="error.statusCode === 404">
+                            <span class="MyName"> {{ pageNotFound }} </span>
+                        </h1>
+                        <h1 class="font1" v-else>
+                            <span class="MyName"> {{ otherError }} </span>
+                        </h1>
+                        <h1 style="font-size: 18px; font-weight: normal;" class="font1">Trabajamos para mejorar</h1>
+                        <div class="social-buttons">
+                          <v-btn data-aos="fade-up" data-aos-anchor-placement="top-bottom" block class="font1 mt-5" style="background-color: #ff0000;" href="/">Regresar al inicio</v-btn>
+                        </div>
+                    </v-col>
+                    <v-col align="center" justify="center" cols="10" md="5" lg="5" sm="10" class="mb-10 " data-aos="fade-up-left" data-aos-duration="4000">
+                        <img class="foto"  width="450" src="/IMG/Desarrollador_web_Alejandro_Sastouqe.webp">
+                    </v-col>
+                </v-row>
+            </v-col>
+        </v-row>
+    </div>
   </v-app>
+
 </template>
 
 <script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,9 @@ export default {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
   },
-
+  generate: {
+    fallback: true // Esto es importante para la generación estática
+  },
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
