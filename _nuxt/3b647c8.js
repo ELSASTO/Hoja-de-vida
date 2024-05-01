badb0497758caafdb12ef90b57bd65921af02268
ELSASTO <71051633+ELSASTO@users.noreply.github.com>
2024-05-01 14:24:35 +0000

    Deploying to gh-pages from @ ELSASTO/Hoja-de-vida@c03943d009fd666b3095674f37b7d6739c20626c 🚀

---
+++ _nuxt/3b647c8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{445:function(n,t,e){"use strict";e.r(t);var l=e(84),component=Object(l.a)({},(function(){return(0,this._self._c)("h2",[this._v("CTa")])}),[],!1,null,null,null);t.default=component.exports}}]);
